b93cff9e61bad019ad5e98ae5b9cf1b32f10fa2b
sonam <sonamlakhanpal@gmail.com>
2024-08-26 17:14:20 +0530

    commit on 26-08-2024

---
+++ Tests2024/MaxAndMinNumber.java
@@ -0,0 +1,42 @@
+package Tests2024;
+
+public class MaxAndMinNumber {
+
+
+    public static void main(String[] args) {
+
+        int arr[] = {2,3,9,8,7,0,1};
+
+        // op- find max number and min number
+
+        System.out.println(maxNumber(arr));
+        System.out.println(minNumber(arr));
+
+    }
+
+    private static int minNumber(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+
+            if(arr[i]<min){
+                min = arr[i];
+            }
+        }
+
+        return min;
+    }
+
+    private static int maxNumber(int[] arr) {
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+
+        return max;
+    }
+
+
+}
