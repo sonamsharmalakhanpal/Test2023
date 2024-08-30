ce8ab84c0ebbf076fc15661cfb24516a2a339c81
sonam <sonamlakhanpal@gmail.com>
2024-08-30 15:26:49 +0530

    commit on 30-08-2024

---
+++ Tests2024/CountWords.java
@@ -0,0 +1,28 @@
+package Tests2024;
+
+public class CountWords {
+
+    public static void main(String[] args) {
+        String s = "My name is Sonam";
+
+        System.out.println("The number of count in given scentence is "+countWordlength(s));
+
+    }
+
+    private static int countWordlength(String s) {
+        int count = 0;
+
+        if(s.charAt(0)!=' '){ //increment the count if first word is not empty or space otherwise without this condition check always one less count will come
+            count=1;
+        }
+            for (int i = 0; i < s.length(); i++) {
+
+
+            if ((s.charAt(i) == ' ') && s.charAt(i + 1) != ' ') { // note after == space will come in single quote ' ' and second cond is i+1 not be space
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
---
+++ Tests2024/uniqueSubString.java
@@ -0,0 +1,33 @@
+package Tests2024;
+
+import javax.xml.stream.events.Characters;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class uniqueSubString {
+
+
+    public static void main(String[] args) {
+        String str = "exampleexam";
+        //o/p-exampl
+
+        findUniqueSubstring(str);
+
+
+    }
+
+    private static void findUniqueSubstring(String str) {
+        String longestUniqueSubstring = "";
+
+        HashSet<Character> set = new HashSet<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (!set.contains(str.charAt(i))) {
+                set.add(str.charAt(i));
+                longestUniqueSubstring = longestUniqueSubstring + str.charAt(i);
+            }
+        }
+
+        System.out.println(longestUniqueSubstring);
+
+    }
+}
