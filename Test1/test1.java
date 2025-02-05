073390397bca513d82304a2dec24876edea54182
Sonam Lakhanpal <sonamlakhanpal@gmail.com>
2025-02-05 17:28:51 +0530

    new computer 05-02-2025

--- Test1/test1.java
+++ Test1/test1.java
@@ -4,5 +4,8 @@ public class test1 {
 
     public static void main(String[] args) {
         System.out.println("JMPJ test");
+
+
+        System.out.println("Jai Maa PARWATI ji Thankyou so much for your blessings");
     }
 }
